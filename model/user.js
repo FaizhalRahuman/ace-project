b904014dc406088a6683d12f255012d6f61bf41a
faizhal <faizhalrahuman2004@gmail.com>
2024-08-14 11:40:37 +0530

    hi vino

--- model/user.js
+++ model/user.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
-var url='mongoose://localhost:27017/employeeManagement';
+var url='mongodb://faizhal:faizhal@ac-kpiqdwz-shard-00-00.jzd7ttq.mongodb.net:27017,ac-kpiqdwz-shard-00-01.jzd7ttq.mongodb.net:27017,ac-kpiqdwz-shard-00-02.jzd7ttq.mongodb.net:27017/?replicaSet=atlas-b52yd7-shard-0&ssl=true&authSource=admin';
 
 
 const userSchema=new mongoose.Schema({
